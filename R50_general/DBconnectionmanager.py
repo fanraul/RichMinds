91d50035ee0456c5d84878d74c5a5733bf709831
fanraul <fanraul@gmail.com>
2018-04-02 00:04:06 +0800

    minor changes

--- R50_general/DBconnectionmanager.py
+++ R50_general/DBconnectionmanager.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
-from R50_general.general_constants import ls_hostname_DEV,ls_hostname_PRD,ls_hostname_HF_PRD
+from R50_general.general_constants import ls_hostname_DEV,ls_hostname_PRD,ls_hostname_OLAP
 import socket
 # remote database
 # DB_connection_string = 'mssql+pyodbc://Richmind:121357468@Richmind_Remote'
 # local database
 
 RICHMINDS_DEV_conn_str = 'mssql+pyodbc://Richmind:121357468@Richmind'
+RICHMINDS_OLAP_conn_str = 'mssql+pyodbc://Richmind:121357468@richmind@richmind_OLAP'
 RICHMINDS_PRD_conn_str = 'mssql+pyodbc://Richmind:121357468@richmind@Richmind_PRD'
 RICHMINDS_HF_PRD_conn_str = 'mssql+pyodbc://Richmind:121357468@richmind@RICHMIND_HF_PRD'
 
@@ -18,8 +19,11 @@ elif hostname in ls_hostname_PRD:
     # HF_DB_connection_string = 'mssql+pyodbc://Richmind:121357468@HF_Richmind_PRD'
     # TODO: 目前HF数据库是内网,prd无法连接,同时prd也无需连接HF,故用他自己的链接作为HF链接,保证不报错进行.
     HF_DB_connection_string = RICHMINDS_PRD_conn_str
-elif hostname in ls_hostname_HF_PRD:
-    DB_connection_string = RICHMINDS_PRD_conn_str
+# elif hostname in ls_hostname_HF_PRD:
+#     DB_connection_string = RICHMINDS_PRD_conn_str
+#     HF_DB_connection_string = RICHMINDS_HF_PRD_conn_str
+elif hostname in ls_hostname_OLAP:
+    DB_connection_string = RICHMINDS_OLAP_conn_str
     HF_DB_connection_string = RICHMINDS_HF_PRD_conn_str
 else:
     assert 0==1,'please update the hostname to decide which SQL server to link!'
